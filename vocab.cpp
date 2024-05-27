62f678a75f66c4e35d434e25a49c56c8a1a5cdb9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-05-27 18:22:13 -0400

    refactor: Add CLI option handling to enable passing in file paths as arguments

--- vocab.cpp
+++ vocab.cpp
@@ -1,9 +1,29 @@
 #include <cstring>
 #include <fstream>
+#include <getopt.h>
 #include <nlohmann/json.hpp>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+    const char* const   short_options = "f:";
+    const struct option long_options[]
+        = {{"registry-file", required_argument, nullptr, 'f'}, {nullptr}};
+
+    int  opt;
+    char registryFilePath[1024];
+
+    while ((opt = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1) {
+        switch (opt) {
+            case 'f':
+                strcpy(registryFilePath, optarg);
+                break;
+
+            default:
+                puts("Usage: vocab [-f <file>]");
+                return 1;
+        }
+    }
+
     puts("Hello, world!");
-    std::ifstream  f("registry.json");
+    std::ifstream  f(registryFilePath);
     nlohmann::json data = nlohmann::json::parse(f);
 }
