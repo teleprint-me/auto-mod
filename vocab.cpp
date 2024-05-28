5bc752482ec319c00959382d643fcda85150c2aa
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-05-27 22:22:27 -0400

    refactor: Add a draft for processing the model registry

--- vocab.cpp
+++ vocab.cpp
@@ -33,9 +33,50 @@ int main(int argc, char* argv[]) {
     }
 
     for (const auto &config : data) {
-        std::cout << "Model Repository:\t" << config["model_repo"] << "\n";
+        const std::string modelArch = config["model_arch"];
 
-        std::cout << "Model Architecture:\t" << config["model_arch"] << '\n';
+        if (modelArch == "stablelm") {
+            std::cout << "Model Architecture:\t" << modelArch << '\n';
+
+            // normalizer and pre_tokenizer are either null or an object
+            const nlohmann::json normalizer    = config["normalizer"];
+            const nlohmann::json pre_tokenizer = config["pre_tokenizer"];
+
+            // Handle the cases where a key may or may not be present and have a null value
+            if (!normalizer.is_null()) {
+                // NOTE: The normalizer type key is not guaranteed
+                const std::string norm_type = normalizer["type"];
+                // i don't know how to check for a "truthy" string in C++
+                std::cout << "Normalizer Type:" << norm_type << std::endl;
+                // sequence is an array
+                if (0 == norm_type.compare("Sequence")) {
+                    // extract the array
+                    // each element in the array is an object
+                } else {
+                    // otherwise norm_type is an object potentially containing properties
+                    // this varies from model to model
+                    // maybe we can just dump the entire object?
+                    // i don't know yet
+                }
+            }
+
+            if (!pre_tokenizer.is_null()) {
+                // NOTE: The pre_tokenizer type is not guaranteed
+                const std::string pre_tokenizer_type = pre_tokenizer["type"];
+                // i don't know how to check for a "truthy" string in C++
+                std::cout << "Pre-tokenizer Type:" << pre_tokenizer_type << std::endl;
+                // sequence is an array
+                if (0 == pre_tokenizer_type.compare("Sequence")) {
+                    // extract the array
+                    // each element in the array is an object
+                } else {
+                    // otherwise pre_tokenizer_type is an object potentially containing properties
+                    // this varies from model to model
+                    // maybe we can just dump the entire object?
+                    // i don't know yet
+                }
+            }
+        }
     }
 
     return 0;
