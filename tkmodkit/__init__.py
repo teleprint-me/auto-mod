92d608a6a10a25095e629ea52703dd06b82fa4a2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-05-29 17:15:52 -0400

    chore: Allow flexible importing

--- tkmodkit/__init__.py
+++ tkmodkit/__init__.py
@@ -1,5 +1,8 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent / "gguf"))
-import gguf
+try:
+    import gguf
+except ImportError:
+    sys.path.insert(0, str(Path(__file__).parent / "gguf"))
+    import gguf
