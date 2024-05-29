79e9a0153a408650a70df2047482b93489a1c6cf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-05-29 01:03:30 -0400

    feat: Add threading to speed up dataset processing for conversion from parquet to plaintext

--- data2txt.py
+++ data2txt.py
@@ -5,7 +5,8 @@ data2txt.py - convert parquet files to plaintext
 
 import argparse
 import os
-import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
 
 from datasets import load_dataset, Dataset, DatasetDict
 from pathlib import Path
@@ -13,52 +14,60 @@ from tqdm import tqdm
 
 
 def parquet_to_plaintext(path: Path) -> None:
-    # Convert parquet files into plaintext files
-    dataset = load_dataset(str(path))  # DatasetDict
-    for index, subset in enumerate(dataset.items()):  # int, tuple
-        name, rows = subset  # str, Dataset
-        print(f"Converting subset {name} to plaintext")
+    dataset: DatasetDict = load_dataset(str(path))
 
-        # Concatenate all text features for a given subset
+    def process_subset(data: tuple[str, Dataset]):
+        name, subset = data
         text = ""
-        for row in tqdm(rows, total=len(rows)):  # str
-            text += "\n".join(f"{row['text'].strip().split('\n')}")
 
-        # Write the concatenated text to a plaintext file
-        output_file_path = path / f"{name}-{index:05}-of-{len(dataset.keys()):05}.txt"
+        for row in tqdm(subset, total=len(subset)):
+            text += row["text"]
+
+        output_file_path = path / f"wiki-{name}.raw"
+
         print(f"Writing {output_file_path}")
 
         try:
             with open(output_file_path, "w") as f:
                 f.write(text)
+
         except (OSError, IOError) as e:
             print(f"Error while writing to file '{output_file_path}': {e}")
 
+    with ThreadPoolExecutor(max_workers=cpu_count()) as pool:
+        pool.map(process_subset, dataset.items())
+
 
 def get_raw_file_paths(path: Path) -> list[str]:
-    return [
-        ent.path for ent in os.scandir(str(path)) if Path(ent.path).suffix == ".txt"
-    ]
+    file_paths = []
+    for entry in os.scandir(str(path)):
+        if Path(entry.path).suffix == ".raw":
+            file_paths.append(entry.path)
+    return file_paths
 
 
 def main():
-    parser = argparse.ArgumentParser(help="Convert parquet files to plaintext")
+    parser = argparse.ArgumentParser()
+
     parser.add_argument(
         "-d",
         "--dir-path",
         default="data/wikitext-103-raw-v1",
         help="The directory to the dataset",
     )
+
     args = parser.parse_args()
 
     path = Path(args.dir_path)
+
     if not path.is_dir():
         raise FileNotFoundError(f"{path} does not exist!")
 
     # Load the dataset
     parquet_to_plaintext(path)
-    raw_files = get_raw_file_paths(path)
-    print(raw_files)
+
+    for raw in get_raw_file_paths(path):
+        print(raw)
 
 
 if __name__ == "__main__":
