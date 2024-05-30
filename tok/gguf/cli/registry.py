b743c5850c293c93090279558bf03e2d40f701b0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-05-29 22:02:28 -0400

    chore: Apply updated constants
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- tok/gguf/cli/registry.py
+++ tok/gguf/cli/registry.py
@@ -5,16 +5,16 @@ import argparse
 import json
 import logging
 
-from gguf.constants import (
-    HF_TOKENIZER_BPE_FILES,
-    HF_TOKENIZER_SPM_FILES,
-    MODEL_ARCH,
-    MODEL_ARCH_NAMES,
+from ..constants import (
+    MODEL_TOKENIZER_BPE_FILES,
+    MODEL_TOKENIZER_SPM_FILES,
+    GGUF_MODEL_ARCH,
+    GGUF_MODEL_ARCH_NAMES,
     ModelFileExtension,
-    PreTokenizerType,
-    VocabType,
+    ModelPreTokenizerType,
+    ModelTokenizerType,
 )
-from gguf.huggingface_hub import HFHubModel, HFHubTokenizer
+from ..huggingface_hub import HFHubModel, HFHubTokenizer
 
 logger = logging.getLogger(__file__)
 
@@ -27,10 +27,10 @@ logger = logging.getLogger(__file__)
 #
 # Field Descriptions
 #   - `model_repo` (str): The HuggingFace endpoint or local path to the models repository
-#   - `model_arch` (MODEL_ARCH): Model architecture type
+#   - `model_arch` (GGUF_MODEL_ARCH): Model architecture type
 #   - `model_parts` (int): Number of parts required to join the model during conversion
 #   - `model_type` (FileFormatType): File format for the Hugging Face model files
-#   - `vocab_type` (VocabType): Vocabulary type used by the tokenizer
+#   - `vocab_type` (ModelTokenizerType): Vocabulary type used by the tokenizer
 #   - `vocab_pre` (Optional[Tuple[str]]): Tuple of pre-tokenizer pattern strings for this model
 #   - `vocab_files` (Tuple[str]): Tuple of file names required to extract vocabulary and other metadata
 #
@@ -41,249 +41,249 @@ HF_MODEL_MAP = (
     # SPM (Sentence Piece Models): Default to Byte Level Pre-tokenization.
     {
         "model_repo": "meta-llama/Llama-2-7b-hf",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 2,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     {
         "model_repo": "mistralai/Mistral-7B-Instruct-v0.1",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 2,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     {
         "model_repo": "mistralai/Mistral-7B-Instruct-v0.2",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 3,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     {  # NOTE: Mistral v0.3 has a 'tokenizer.model.v3' file
         "model_repo": "mistralai/Mistral-7B-Instruct-v0.3",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 3,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     {
         "model_repo": "mistralai/Mixtral-8x7B-Instruct-v0.1",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 8,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     {
         "model_repo": "microsoft/Phi-3-mini-4k-instruct",
-        "model_arch": MODEL_ARCH.PHI3,
+        "model_arch": GGUF_MODEL_ARCH.PHI3,
         "model_parts": 2,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.SPM.value,
+        "vocab_type": ModelTokenizerType.SPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_SPM_FILES,
+        "vocab_files": MODEL_TOKENIZER_SPM_FILES,
     },
     # WPM (Word Piece Models): Default to Byte Level Pre-tokenization.
     # NOTE: BERT Normalization and Pre-tokenization rules differ from Byte Level Pre-tokenization.
     {
         "model_repo": "BAAI/bge-small-en-v1.5",
-        "model_arch": MODEL_ARCH.BERT,
+        "model_arch": GGUF_MODEL_ARCH.BERT,
         "model_parts": 1,
         "model_type": ModelFileExtension.BIN.value,
-        "vocab_type": VocabType.WPM.value,
+        "vocab_type": ModelTokenizerType.WPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "jinaai/jina-embeddings-v2-base-en",
-        "model_arch": MODEL_ARCH.JINA_BERT_V2,
+        "model_arch": GGUF_MODEL_ARCH.JINA_BERT_V2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.WPM.value,
+        "vocab_type": ModelTokenizerType.WPM.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     # BPE (Byte Pair Encoding Models): Default is Byte Level Pre-tokenization
     {
         "model_repo": "meta-llama/Meta-Llama-3-8B",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 4,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "tiiuae/falcon-7b",
-        "model_arch": MODEL_ARCH.FALCON,
+        "model_arch": GGUF_MODEL_ARCH.FALCON,
         "model_parts": 2,
         "model_type": ModelFileExtension.BIN.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "deepseek-ai/deepseek-llm-7b-base",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 2,
         "model_type": ModelFileExtension.BIN.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "deepseek-ai/deepseek-coder-6.7b-base",
-        "model_arch": MODEL_ARCH.LLAMA,
+        "model_arch": GGUF_MODEL_ARCH.LLAMA,
         "model_parts": 2,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "mosaicml/mpt-7b",
-        "model_arch": MODEL_ARCH.MPT,
+        "model_arch": GGUF_MODEL_ARCH.MPT,
         "model_parts": 2,
         "model_type": ModelFileExtension.BIN.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     #
     # BPE: STARCODER
     #
     {
         "model_repo": "bigcode/starcoder2-3b",
-        "model_arch": MODEL_ARCH.STARCODER2,
+        "model_arch": GGUF_MODEL_ARCH.STARCODER2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "smallcloudai/Refact-1_6-base",
-        "model_arch": MODEL_ARCH.REFACT,
+        "model_arch": GGUF_MODEL_ARCH.REFACT,
         "model_parts": 1,
         "model_type": ModelFileExtension.BIN.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "CohereForAI/c4ai-command-r-v01",
-        "model_arch": MODEL_ARCH.COMMAND_R,
+        "model_arch": GGUF_MODEL_ARCH.COMMAND_R,
         "model_parts": 15,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     #
     # BPE: QWEN
     #
     {
         "model_repo": "Qwen/Qwen1.5-7B",
-        "model_arch": MODEL_ARCH.QWEN2,
+        "model_arch": GGUF_MODEL_ARCH.QWEN2,
         "model_parts": 4,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "stabilityai/stablelm-2-zephyr-1_6b",
-        "model_arch": MODEL_ARCH.STABLELM,
+        "model_arch": GGUF_MODEL_ARCH.STABLELM,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     #
     # BPE: GPT-2
     #
     {
         "model_repo": "openai-community/gpt2",
-        "model_arch": MODEL_ARCH.GPT2,
+        "model_arch": GGUF_MODEL_ARCH.GPT2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "allenai/OLMo-1.7-7B-hf",
-        "model_arch": MODEL_ARCH.OLMO,
+        "model_arch": GGUF_MODEL_ARCH.OLMO,
         "model_parts": 6,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     # {  # NOTE: I don't have access to this model
     #     "model_repo": "databricks/dbrx-base",
-    #     "model_arch": MODEL_ARCH.DBRX,
+    #     "model_arch": GGUF_MODEL_ARCH.DBRX,
     #     "model_parts": 0,
     #     "model_type": ModelFileExtension.SAFETENSORS.value,
-    #     "vocab_type": VocabType.BPE.value,
+    #     "vocab_type": ModelTokenizerType.BPE.value,
     #     "vocab_pre": None,
-    #     "vocab_files": HF_TOKENIZER_BPE_FILES,
+    #     "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     # },
     {  # NOTE: RoBERTa post processor
         "model_repo": "jinaai/jina-embeddings-v2-base-es",
-        "model_arch": MODEL_ARCH.JINA_BERT_V2,
+        "model_arch": GGUF_MODEL_ARCH.JINA_BERT_V2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {  # NOTE: RoBERTa post processor
         "model_repo": "jinaai/jina-embeddings-v2-base-de",
-        "model_arch": MODEL_ARCH.JINA_BERT_V2,
+        "model_arch": GGUF_MODEL_ARCH.JINA_BERT_V2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {  # NOTE: Phi-1 is compatible with GPT-2 arch and vocab
         "model_repo": "microsoft/phi-1",
-        "model_arch": MODEL_ARCH.PHI2,
+        "model_arch": GGUF_MODEL_ARCH.PHI2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "microsoft/phi-1_5",
-        "model_arch": MODEL_ARCH.PHI2,
+        "model_arch": GGUF_MODEL_ARCH.PHI2,
         "model_parts": 1,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
     {
         "model_repo": "microsoft/phi-2",
-        "model_arch": MODEL_ARCH.PHI2,
+        "model_arch": GGUF_MODEL_ARCH.PHI2,
         "model_parts": 2,
         "model_type": ModelFileExtension.SAFETENSORS.value,
-        "vocab_type": VocabType.BPE.value,
+        "vocab_type": ModelTokenizerType.BPE.value,
         "vocab_pre": None,
-        "vocab_files": HF_TOKENIZER_BPE_FILES,
+        "vocab_files": MODEL_TOKENIZER_BPE_FILES,
     },
 )
 
@@ -327,7 +327,7 @@ for model in HF_MODEL_MAP:
     model_arch = model["model_arch"]
     vocab_type = model["vocab_type"]
 
-    print("HUB_REPO:", model_repo, "LLAMA_ARCH:", MODEL_ARCH_NAMES[model_arch])
+    print("HUB_REPO:", model_repo, "LLAMA_ARCH:", GGUF_MODEL_ARCH_NAMES[model_arch])
 
     hub_model.download_all_vocab_files(
         model_repo=model_repo,
@@ -336,7 +336,7 @@ for model in HF_MODEL_MAP:
     # log the downloaded results
     hub_tokenizer.log_tokenizer_json_info(model_repo)
 
-    model["model_arch"] = MODEL_ARCH_NAMES[model_arch]
+    model["model_arch"] = GGUF_MODEL_ARCH_NAMES[model_arch]
 
     normalizer = hub_tokenizer.get_normalizer(model_repo)
     # NOTE: Normalizer may be one of null, Sequence, NFC, NFD, NFKC, NFKD...
