52d5f659d3d5282aad286dfdf06d11cbccb91fca
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-03 19:53:14 -0400

    refactor: Apply use of generic mime types for forward compatibility and simplify FileType implementation for general flexibility

--- tok/file_type.py
+++ tok/file_type.py
@@ -3,94 +3,59 @@ import json
 
 
 class FileType:
+    BIN = {
+        b"\n\x0e\n\x05": "application/spm",  # not guaranteed
+        b"GGUF": "application/gguf",  # ggml/llama.cpp; little endian
+        b"PK\x03\x04": "application/zip",  # TODO: pytorch/other; compressed
+    }
+
     @staticmethod
-    def is_binary(file_name: str) -> bool:
-        """
-        Determines if a given file is a binary tokenizer model.
+    def _read_json(file_name: str) -> None | str:
+        with open(file_name, "r", encoding="utf-8") as check_file:
+            data = json.load(check_file)
 
-        This method exploits the fact that attempting to read a binary file
-        using the default 'r' mode raises a UnicodeDecodeError exception,
-        allowing us to make an informed decision based on the observed byte
-        sequence.
+            # Check for the presence of a "model" key in the loaded JSON data
+            if "model" in data:
+                return "application/json"
 
-        :param file_name: The path to the file to be checked.
-        :return: A boolean value indicating whether the given file is a binary
-                 SentencePiece tokenizer model or not.
+        raise LookupError("Unrecognized json model format")
 
-        :citation: https://stackoverflow.com/a/51495076/15147156
-        """
+    @staticmethod
+    def _read_text(file_name: str) -> None | str:
+        # Attempt to read the file as plaintext using default 'r' mode
+        with open(file_name, "r", encoding="utf-8") as file:
+            file.read()
 
+            # If we reached this point, it means the file is not a
+            # binary file and raises a UnicodeDecodeError exception
+            # when attempting to read it in text mode
+            return "text/plain"
+
+        raise LookupError("Unrecognized plaintext model format")
+
+    @staticmethod
+    def _read_binary(file_name: str) -> None | str:
+        with open(file_name, "rb") as file:
+            header = file.read(4)
+
+            if len(header) == 0 or header not in FileType.BIN.keys():
+                return None
+
+            return FileType.BIN[header]
+
+        raise LookupError("Unrecognized binary model format")
+
+    @staticmethod
+    def get_type(file_name: str) -> str:
         try:
-            # Attempt to read the file as plaintext using default 'r' mode
-            with open(file_name, "r") as file:
-                file.read()
-
-                # If we reached this point, it means the file is not a
-                # binary file and raises a UnicodeDecodeError exception
-                # when attempting to read it in text mode
-                return False
-
-        except UnicodeDecodeError:
-            with open(file_name, "rb") as file:
-                header = file.read(4)
-
-                # Check for the specific byte sequence associated with
-                # SentencePiece models
-                if header == b"\n\x0e\n\x05":
-                    return True
-
-                # GGUF models
-                if header == b'GGUF':  # little endian
-                    return True
-
-                # If the file is not a binary SentencePiece model, raise an exception
-                raise LookupError("Unrecognized binary tokenizer model format")
-
-    @staticmethod
-    def is_json(file_name: str) -> bool:
-        """
-        Determines if a given file is a JSON tokenizer file.
-
-        This method checks for the presence of a "model" key in the JSON data
-        loaded from the file, which is a common structure for Hugging Face
-        tokenizer configuration files. This method can be extended for other
-        implementations as well.
-
-        :param file_name: The path to the file to be checked.
-        :return: A boolean value indicating whether the given file is a
-        tokenizer JSON file or not.
-
-        :raises: json.JSONDecodeError if the file cannot be read as JSON.
-        """
-
-        try:  # Hack to "short-circuit" complex filetype associations
-            with open(file_name, "r") as check_file:
-                data = json.load(check_file)
-
-                # Check for the presence of a "model" key in the loaded JSON data
-                if "model" in data:
-                    return True
-
-                raise LookupError("Unrecognized json tokenizer model format")
+            return FileType._read_json(file_name)
 
         except json.JSONDecodeError:
-            return False  # Not a valid JSON file
+            return FileType._read_text(file_name)
 
-    def is_plaintext(self, file_name: str) -> bool:
-        return not self.is_binary(file_name)
+        except UnicodeDecodeError:
+            return FileType._read_binary(file_name)
 
-    def get_file_type(self, file_name: str) -> str:
-        try:
-            if self.is_binary(file_name):
-                return "binary"
-            # check if json first because json is plaintext too
-            elif self.is_json(file_name):
-                return "json"
-            # check if plaintext if not json
-            elif self.is_plaintext(file_name):
-                return "plaintext"
-            else:  # we don't know what the file type is
-                return "unknown"
         except (IOError, OSError) as e:
             raise FileNotFoundError(f"File '{file_name}' not found.") from e
 
