0a753a4d4b9d41fa95585888796785286828147c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-03 17:46:08 -0400

    refactor: Apply a mapping to enable maintainability if other specs are added over time

--- specs.sh
+++ specs.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-mkdir specs
-wget https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT -O specs/zip.txt
-wget https://raw.githubusercontent.com/ggerganov/ggml/master/docs/gguf.md -O specs/gguf.md
-wget https://raw.githubusercontent.com/huggingface/hub-docs/main/modelcard.md -O specs/model-cards.md
-wget https://github.com/huggingface/safetensors/raw/main/docs/safetensors.schema.json -O specs/safetensors.json
+declare -A URL_MAP=(
+    ["zip"]="https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT"
+    ["gguf"]="https://raw.githubusercontent.com/ggerganov/ggml/master/docs/gguf.md"
+    ["model-cards"]="https://raw.githubusercontent.com/huggingface/hub-docs/main/modelcard.md"
+    ["safetensors"]="https://github.com/huggingface/safetensors/raw/main/docs/safetensors.schema.json"
+)
+
+declare -A OUTPUT_MAP=(
+    ["zip"]="specs/zip.txt"
+    ["gguf"]="specs/gguf.md"
+    ["model-cards"]="specs/model-cards.md"
+    ["safetensors"]="specs/safetensors.json"
+)
+
+for key in "${!URL_MAP[@]}"
+do
+  wget ${URL_MAP[$key]} -O ${OUTPUT_MAP[$key]}
+done
