74570a9979193704af6b40c04d811c9db37473ce
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-03 19:42:24 -0400

    tests: Refactor class FileType tests and attempt to follow mime type conventions for potential forward compatibility

--- tests/test_file_type.py
+++ tests/test_file_type.py
@@ -11,19 +11,27 @@ def file_type() -> FileType:
 
 
 def test_bpe_file(file_type):
-    assert file_type.get_file_type("tokenizers/bpe/tokenizer.model") == "plaintext"
+    assert file_type.get_file_type("tokenizers/bpe/tokenizer.model") == "text/plain"
 
 
 def test_sentencepiece_model(file_type):
-    assert file_type.get_file_type("tokenizers/spm/tokenizer.model") == "binary"
+    assert (
+        file_type.get_file_type("tokenizers/spm/tokenizer.model") == "application/spm"
+    )
 
 
 def test_json_file(file_type):
-    assert file_type.get_file_type("tokenizers/hf/tokenizer.json") == "json"
+    assert file_type.get_file_type("tokenizers/hf/tokenizer.json") == "application/json"
+
+
+def test_gguf_file(file_type):
+    assert (
+        file_type.get_file_type("tokenizers/gguf/tokenizer.model") == "application/gguf"
+    )
 
 
 def test_unknown_file(file_type):
-    assert file_type.get_file_type("test_unknown_file.txt") == "unknown"
+    assert file_type.get_file_type("test_unknown_file.txt") is None
 
 
 def test_nonexistent_file(file_type):
@@ -36,8 +44,10 @@ def test_plaintext_file(file_type):
     path = "test_plain_file.txt"
     with open(path, "w") as f:
         f.write("This is just a text 😸")
+
     # test file before cleanup
-    assert file_type.get_file_type(path) == "plaintext"
+    assert file_type.get_file_type(path) == "text/plain"
+
     # cleanup file after test
     os.remove(path)
 
@@ -46,5 +56,9 @@ def test_empty_file(file_type):
     path = "temp_empty_file.txt"
     with open(path, "w") as f:
         f.write("")
-    assert file_type.get_file_type(path) == "plaintext"
+
+    # test file before cleanup
+    assert file_type.get_file_type(path) == "text/plain"
+
+    # cleanup file after test
     os.remove(path)
