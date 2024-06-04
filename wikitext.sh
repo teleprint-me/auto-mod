965ab40b03baa790ed7b61593e51658b3727f01e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-03 21:01:46 -0400

    patch: Fix download references for wikitext 103 raw v1

--- wikitext.sh
+++ wikitext.sh
@@ -28,15 +28,15 @@ function wikitext_103_raw_v1() {
 
     declare -A WIKI_MAP=(
         ["test"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/test-00000-of-00001.parquet?download=true'
-        ["train_1"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/train-00000-of-00002.parquet?download=true'
-        ["train_2"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/train-00001-of-00002.parquet?download=true'
+        ["train_0"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/train-00000-of-00002.parquet?download=true'
+        ["train_1"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/train-00001-of-00002.parquet?download=true'
         ["validation"]='https://huggingface.co/datasets/Salesforce/wikitext/resolve/main/wikitext-103-raw-v1/validation-00000-of-00001.parquet?download=true'
     )
 
     declare -A OUTPUT_MAP=(
         ["test"]="${WIKI_PATH}/test-00000-of-00001.parquet"
-        ["train_1"]="${WIKI_PATH}/train-00000-of-00002.parquet"
-        ["train_2"]="${WIKI_PATH}/train-00000-of-00002.parquet"
+        ["train_0"]="${WIKI_PATH}/train-00000-of-00002.parquet"
+        ["train_1"]="${WIKI_PATH}/train-00001-of-00002.parquet"
         ["validation"]="${WIKI_PATH}/validation-00000-of-00001.parquet"
     )
 
