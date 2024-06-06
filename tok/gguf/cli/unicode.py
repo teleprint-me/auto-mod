0595abeb46aba7eae436c8457dea1838be060b1b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-06 17:02:27 -0400

    refactor: Add dataclass for handling cpt ranges and integrate into CodepointProcessor
    - Prepare script for CodepointProcessor integration
    - Restructure executable code and only execute as a script

--- tok/gguf/cli/unicode.py
+++ tok/gguf/cli/unicode.py
@@ -19,9 +19,6 @@ class CodepointFlags(ctypes.Structure):
     ]
 
 
-assert ctypes.sizeof(CodepointFlags) == 2
-
-
 @dataclasses.dataclass
 class UnicodeTable:
     whitespace: list[int] = []
@@ -30,9 +27,17 @@ class UnicodeTable:
     nfd: list[tuple[int, int]] = []
 
 
+@dataclasses.dataclass
+class CodepointRanges:
+    flags: list[tuple[int, int]]
+    nfd: list[tuple[int, int, int]]
+
+
 class CodepointProcessor:
     def __init__(self, max_codepoints: None | int = 0x110000):
+        # Set the unicode upper limit
         self.MAX_CODEPOINTS = max_codepoints
+
         # Regular expressions for various Unicode character categories
         self._regexes = {
             "is_number": regex.compile(r"\p{N}"),
@@ -45,16 +50,21 @@ class CodepointProcessor:
             "is_whitespace": regex.compile(r"\s"),
         }
 
+        # Set the unicode components
         self._codepoint_flags = (CodepointFlags * self.MAX_CODEPOINTS)()
-
+        self._codepoint_ranges = CodepointRanges()
         self._unicode_table = UnicodeTable()
 
     @property
-    def flags(self) -> CodepointFlags:
+    def codepoint_flags(self) -> CodepointFlags:
         return self._codepoint_flags
 
     @property
-    def table(self) -> UnicodeTable:
+    def codepoint_ranges(self) -> CodepointRanges:
+        return self._codepoint_ranges
+
+    @property
+    def unicode_table(self) -> UnicodeTable:
         return self._unicode_table
 
     def process_unicode(self):
@@ -82,6 +92,7 @@ class CodepointProcessor:
             self.set_nfd_table(codepoint, char)
 
     def set_whitespace_table(self, codepoint: int, char: str):
+        # whitespace
         regex = self._regexes["is_whitespace"]
         if bool(regex.match(char)):
             self._unicode_table.whitespace.append(codepoint)
@@ -104,67 +115,74 @@ class CodepointProcessor:
         if codepoint != norm:
             self._unicode_table.nfd.append((codepoint, norm))
 
+    def group_flag_ranges(self):
+        # group ranges with same flags
+        self._codepoint_ranges.flags = [(0, self._codepoint_flags[0])]  # start, flags
+        for codepoint, flags in enumerate(self._codepoint_flags):
+            if bytes(flags) != bytes(self._codepoint_ranges.flags[-1][1]):
+                self._codepoint_ranges.flags.append((codepoint, flags))
+        self._codepoint_ranges.flags.append((self.MAX_CODEPOINTS, CodepointFlags()))
 
-# group ranges with same flags
-ranges_flags = [(0, codepoint_flags[0])]  # start, flags
-for codepoint, flags in enumerate(codepoint_flags):
-    if bytes(flags) != bytes(ranges_flags[-1][1]):
-        ranges_flags.append((codepoint, flags))
-ranges_flags.append((MAX_CODEPOINTS, CodepointFlags()))
-
-# group ranges with same nfd
-ranges_nfd = [(0, 0, 0)]  # start, last, nfd
-for codepoint, norm in table_nfd:
-    start = ranges_nfd[-1][0]
-    if ranges_nfd[-1] != (start, codepoint - 1, norm):
-        ranges_nfd.append(None)
-        start = codepoint
-    ranges_nfd[-1] = (start, codepoint, norm)
+    def group_nfd_ranges(self):
+        # group ranges with same nfd
+        self._codepoint_ranges.nfd = [(0, 0, 0)]  # start, last, nfd
+        for codepoint, norm in self.unicode_table.nfd:
+            start = self._codepoint_ranges.nfd[-1][0]
+            if self._codepoint_ranges.nfd[-1] != (start, codepoint - 1, norm):
+                self._codepoint_ranges.nfd.append(None)
+                start = codepoint
+            self._codepoint_ranges.nfd[-1] = (start, codepoint, norm)
 
 
 # Generate 'unicode-data.cpp'
 
 
-def out(line=""):
-    print(line, end="\n")  # noqa
+def main():
+    assert ctypes.sizeof(CodepointFlags) == 2
+
+    def out(line=""):
+        print(line, end="\n")  # noqa
+
+    out(
+        """\
+    // generated with scripts/gen-unicode-data.py
+
+    #include "unicode-data.h"
+
+    #include <cstdint>
+    #include <vector>
+    #include <unordered_map>
+    #include <unordered_set>
+    """
+    )
+
+    out(
+        "const std::vector<std::pair<uint32_t, uint16_t>> unicode_ranges_flags = {  // start, flags // last=next_start-1"
+    )
+    for codepoint, flags in ranges_flags:
+        flags = int.from_bytes(bytes(flags), "little")
+        out("{0x%06X, 0x%04X}," % (codepoint, flags))
+    out("};\n")
+
+    out("const std::unordered_set<uint32_t> unicode_set_whitespace = {")
+    out(", ".join("0x%06X" % cpt for cpt in table_whitespace))
+    out("};\n")
+
+    out("const std::unordered_map<uint32_t, uint32_t> unicode_map_lowercase = {")
+    for tuple in table_lowercase:
+        out("{0x%06X, 0x%06X}," % tuple)
+    out("};\n")
+
+    out("const std::unordered_map<uint32_t, uint32_t> unicode_map_uppercase = {")
+    for tuple in table_uppercase:
+        out("{0x%06X, 0x%06X}," % tuple)
+    out("};\n")
+
+    out("const std::vector<range_nfd> unicode_ranges_nfd = {  // start, last, nfd")
+    for triple in ranges_nfd:
+        out("{0x%06X, 0x%06X, 0x%06X}," % triple)
+    out("};\n")
 
 
-out(
-    """\
-// generated with scripts/gen-unicode-data.py
-
-#include "unicode-data.h"
-
-#include <cstdint>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-"""
-)
-
-out(
-    "const std::vector<std::pair<uint32_t, uint16_t>> unicode_ranges_flags = {  // start, flags // last=next_start-1"
-)
-for codepoint, flags in ranges_flags:
-    flags = int.from_bytes(bytes(flags), "little")
-    out("{0x%06X, 0x%04X}," % (codepoint, flags))
-out("};\n")
-
-out("const std::unordered_set<uint32_t> unicode_set_whitespace = {")
-out(", ".join("0x%06X" % cpt for cpt in table_whitespace))
-out("};\n")
-
-out("const std::unordered_map<uint32_t, uint32_t> unicode_map_lowercase = {")
-for tuple in table_lowercase:
-    out("{0x%06X, 0x%06X}," % tuple)
-out("};\n")
-
-out("const std::unordered_map<uint32_t, uint32_t> unicode_map_uppercase = {")
-for tuple in table_uppercase:
-    out("{0x%06X, 0x%06X}," % tuple)
-out("};\n")
-
-out("const std::vector<range_nfd> unicode_ranges_nfd = {  // start, last, nfd")
-for triple in ranges_nfd:
-    out("{0x%06X, 0x%06X, 0x%06X}," % triple)
-out("};\n")
+if __name__ == "__main__":
+    main()
