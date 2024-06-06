bc2804700d88a78c9874ba60285f44d296a37f38
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-06 03:05:20 -0400

    refactor: Add UnicodeTable and CodepointProcessor classes to apply SoC pattern

--- tok/gguf/cli/unicode.py
+++ tok/gguf/cli/unicode.py
@@ -2,6 +2,7 @@ import ctypes
 import unicodedata
 
 import regex
+import dataclasses
 
 
 # NOTE: Original script has a typo, e.g. CoodepointFlags
@@ -21,57 +22,87 @@ class CodepointFlags(ctypes.Structure):
 assert ctypes.sizeof(CodepointFlags) == 2
 
 
-MAX_CODEPOINTS = 0x110000
+@dataclasses.dataclass
+class UnicodeTable:
+    whitespace: list[int] = []
+    lowercase: list[tuple[int, int]] = []
+    uppercase: list[tuple[int, int]] = []
+    nfd: list[tuple[int, int]] = []
 
-regex_number = regex.compile(r"\p{N}")
-regex_letter = regex.compile(r"\p{L}")
-regex_separator = regex.compile(r"\p{Z}")
-regex_accent_mark = regex.compile(r"\p{M}")
-regex_punctuation = regex.compile(r"\p{P}")
-regex_symbol = regex.compile(r"\p{S}")
-regex_control = regex.compile(r"\p{C}")
-regex_whitespace = regex.compile(r"\s")
 
-codepoint_flags = (CodepointFlags * MAX_CODEPOINTS)()
-table_whitespace = []
-table_lowercase = []
-table_uppercase = []
-table_nfd = []
+class CodepointProcessor:
+    def __init__(self, max_codepoints: None | int = 0x110000):
+        self.MAX_CODEPOINTS = max_codepoints
+        # Regular expressions for various Unicode character categories
+        self._regexes = {
+            "is_number": regex.compile(r"\p{N}"),
+            "is_letter": regex.compile(r"\p{L}"),
+            "is_separator": regex.compile(r"\p{Z}"),
+            "is_accent_mark": regex.compile(r"\p{M}"),
+            "is_punctuation": regex.compile(r"\p{P}"),
+            "is_symbol": regex.compile(r"\p{S}"),
+            "is_control": regex.compile(r"\p{C}"),
+            "is_whitespace": regex.compile(r"\s"),
+        }
 
-for codepoint in range(MAX_CODEPOINTS):
-    # convert codepoint to unicode character
-    char = chr(codepoint)
+        self._codepoint_flags = (CodepointFlags * self.MAX_CODEPOINTS)()
 
-    # regex categories
-    flags = codepoint_flags[codepoint]
-    flags.is_number = bool(regex_number.match(char))
-    flags.is_letter = bool(regex_letter.match(char))
-    flags.is_separator = bool(regex_separator.match(char))
-    flags.is_accent_mark = bool(regex_accent_mark.match(char))
-    flags.is_punctuation = bool(regex_punctuation.match(char))
-    flags.is_symbol = bool(regex_symbol.match(char))
-    flags.is_control = bool(regex_control.match(char))
-    flags.is_undefined = bytes(flags)[0] == 0
-    assert not flags.is_undefined
+        self._unicode_table = UnicodeTable()
 
-    # whitespaces
-    if bool(regex_whitespace.match(char)):
-        table_whitespace.append(codepoint)
+    @property
+    def flags(self) -> CodepointFlags:
+        return self._codepoint_flags
 
-    # lowercase conversion
-    lower = ord(char.lower()[0])
-    if codepoint != lower:
-        table_lowercase.append((codepoint, lower))
+    @property
+    def table(self) -> UnicodeTable:
+        return self._unicode_table
 
-    # uppercase conversion
-    upper = ord(char.upper()[0])
-    if codepoint != upper:
-        table_uppercase.append((codepoint, upper))
+    def process_unicode(self):
+        for codepoint in range(self.MAX_CODEPOINTS):
+            # convert codepoint to unicode character
+            char = chr(codepoint)
 
-    # NFD normalization
-    norm = ord(unicodedata.normalize("NFD", char)[0])
-    if codepoint != norm:
-        table_nfd.append((codepoint, norm))
+            # regex categories
+            flags = self._codepoint_flags[codepoint]
+            for flag in dir(flags):
+                if flag.startswith("__"):
+                    continue
+
+                regex = self._regexes.get(flag)
+                if regex is not None:
+                    setattr(flags, flag, bool(regex.match(char)))
+                elif flag == "is_undefined":
+                    setattr(flags, flag, bytes(flags)[0] == 0)
+                    message = f"'flags' is undefined for {codepoint} with {char}"
+                    assert not flags.is_undefined, message
+
+            self.set_whitespace_table(codepoint, char)
+            self.set_lowercase_table(codepoint, char)
+            self.set_uppercase_table(codepoint, char)
+            self.set_nfd_table(codepoint, char)
+
+    def set_whitespace_table(self, codepoint: int, char: str):
+        regex = self._regexes["is_whitespace"]
+        if bool(regex.match(char)):
+            self._unicode_table.whitespace.append(codepoint)
+
+    def set_lowercase_table(self, codepoint: int, char: str):
+        # lowercase conversion
+        lower = ord(char.lower()[0])
+        if codepoint != lower:
+            self._unicode_table.lowercase.append((codepoint, lower))
+
+    def set_uppercase_table(self, codepoint: int, char: str):
+        # uppercase conversion
+        upper = ord(char.upper()[0])
+        if codepoint != upper:
+            self._unicode_table.uppercase.append((codepoint, upper))
+
+    def set_nfd_table(self, codepoint: int, char: str):
+        # NFD normalization
+        norm = ord(unicodedata.normalize("NFD", char)[0])
+        if codepoint != norm:
+            self._unicode_table.nfd.append((codepoint, norm))
 
 
 # group ranges with same flags
@@ -80,7 +111,6 @@ for codepoint, flags in enumerate(codepoint_flags):
     if bytes(flags) != bytes(ranges_flags[-1][1]):
         ranges_flags.append((codepoint, flags))
 ranges_flags.append((MAX_CODEPOINTS, CodepointFlags()))
-
 
 # group ranges with same nfd
 ranges_nfd = [(0, 0, 0)]  # start, last, nfd
