b232906617dae85f5ea0bd8977e77b2579bc8013
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-05 23:12:32 -0400

    chore: Apply isort to imports
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- tests/test_llama_tokenizer.py
+++ tests/test_llama_tokenizer.py
@@ -1,8 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
-import os
 from unittest import TestCase
+
 from tok.llama.tokenizer import ChatFormat, Tokenizer
 
 # TOKENIZER_PATH=<path> python -m unittest llama/test_tokenizer.py
