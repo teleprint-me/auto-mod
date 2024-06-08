a02f8ef8dfcac01cd3107b7d26dcaf235893d2d6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-08 00:21:44 -0400

    refactor: Add support for file options

--- src/magic.c
+++ src/magic.c
@@ -6,56 +6,81 @@
 #define MAGIC_HEADER 4
 
 struct magic_file {
-    char* path;
-    FILE* file;
-    char* name[MAGIC_HEADER];
+    char*        path;
+    FILE*        file;
+    char         header[MAGIC_HEADER];
+    unsigned int value;
 };
 
 struct options {
     struct magic_file magic;
 };
 
+const char* const    short_options  = "f:";
+static struct option long_options[] = {{"file", required_argument, 0, 'f'}, {NULL}};
+
 int main(int argc, char* argv[]) {
     if (1 == argc) {
-        printf("Usage: %s [-f <file>]", argv[0]);
+        printf("Usage: %s [-f <file>]\n", argv[0]);
         return 1;
     }
 
-    FILE*        file;
-    unsigned int magic;
-    char         magic_header[MAGIC_HEADER];
+    struct options opts;
+    int            c;
+
+    memset(&opts.magic, '\0', sizeof(struct magic_file));
+
+    while (-1 != (c = getopt_long(argc, argv, short_options, long_options, NULL))) {
+        switch (c) {
+            case 'f':
+                opts.magic.path = optarg;
+                break;
+
+            default:
+                printf("Invalid option: %s\n", optarg);
+                break;
+        }
+    }
+
+    if (NULL == opts.magic.path) {
+        printf("Error: File path not provided.\n");
+        return 1;
+    }
+
+    opts.magic.file = fopen(opts.magic.path, "rb");
 
     // Open the binary file
-    if ((file = fopen("my_binary_file.bin", "rb")) == NULL) {
+    if (NULL == opts.magic.file) {
         printf("Error opening file\n");
         return 1;
     }
 
-    // Read the MAGIC value from the file
-    fread(&magic, sizeof(unsigned int), 1, file);
+    memset(opts.magic.header, '\0', sizeof(opts.magic.header));
 
-    memset(magic_header, '\0', sizeof(magic_header));
+    // Read the MAGIC value from the file
+    fread(&opts.magic.value, sizeof(unsigned int), 1, opts.magic.file);
 
     for (size_t i = 0; i < MAGIC_HEADER; ++i) {
         // Convert each byte to its corresponding ASCII character
-        magic_header[i] = magic & 0xFF ? ((magic >> (8 * i)) & 0xFF) : '\0';
+        opts.magic.header[i]
+            = opts.magic.value & 0xFF ? ((opts.magic.value >> (8 * i)) & 0xFF) : '\0';
     }
 
-    printf("The magic number is: %#x\n", le32toh(magic));
+    printf("The magic number is: %#x\n", le32toh(opts.magic.value));
 
     printf("The magic number as a string: ");
 
     // Print each character of magic_header
     for (size_t i = 0; i < MAGIC_HEADER; ++i) {
-        if (magic_header[i] != '\0') {
-            putchar(magic_header[i]);
+        if (opts.magic.header[i] != '\0') {
+            putchar(opts.magic.header[i]);
         }
     }
 
     printf("\n");
 
     // Close the binary file
-    fclose(file);
+    fclose(opts.magic.file);
 
     return 0;
 }
