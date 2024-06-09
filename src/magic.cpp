671a94f2fd81b80375a89f2c11587dd59f4e5af2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-09 02:57:00 -0400

    refactor: Apply GGML specification guidelines for experimental formats

--- src/magic.cpp
+++ src/magic.cpp
@@ -1,20 +1,8 @@
+#include "magic.h"
+
 #include <endian.h>
 #include <getopt.h>
-#include <stdio.h>
 #include <string.h>
-
-#define MAGIC_HEADER 4
-
-struct magic_file {
-    char*        path;
-    FILE*        file;
-    char         header[MAGIC_HEADER];
-    unsigned int value;
-};
-
-struct options {
-    struct magic_file magic;
-};
 
 const char* const    short_options  = "f:";
 static struct option long_options[] = {{"file", required_argument, 0, 'f'}, {NULL}};
@@ -25,15 +13,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    struct options opts;
-    int            c;
+    struct magic_file_t magic;
+    int                 c;
 
-    memset(&opts.magic, '\0', sizeof(struct magic_file));
+    memset(&magic, '\0', sizeof(struct magic_file_t));
+    memset(&magic.header, '\0', sizeof(struct magic_header_t));
 
     while (-1 != (c = getopt_long(argc, argv, short_options, long_options, NULL))) {
         switch (c) {
             case 'f':
-                opts.magic.path = optarg;
+                magic.path = optarg;
                 break;
 
             default:
@@ -42,45 +31,43 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (NULL == opts.magic.path) {
+    if (NULL == magic.path) {
         printf("Error: File path not provided.\n");
         return 1;
     }
 
-    opts.magic.file = fopen(opts.magic.path, "rb");
+    magic.file = fopen(magic.path, "rb");
 
     // Open the binary file
-    if (NULL == opts.magic.file) {
+    if (NULL == magic.file) {
         printf("Error opening file\n");
         return 1;
     }
 
-    memset(opts.magic.header, '\0', sizeof(opts.magic.header));
-
     // Read the MAGIC value from the file
-    fread(&opts.magic.value, sizeof(unsigned int), 1, opts.magic.file);
+    fread(&magic.header.value, sizeof(unsigned int), 1, magic.file);
 
     for (size_t i = 0; i < MAGIC_HEADER; ++i) {
         // Convert each byte to its corresponding ASCII character
-        opts.magic.header[i]
-            = opts.magic.value & 0xFF ? ((opts.magic.value >> (8 * i)) & 0xFF) : '\0';
+        magic.header.name[i]
+            = magic.header.value & 0xFF ? ((magic.header.value >> (8 * i)) & 0xFF) : '\0';
     }
 
-    printf("The magic number is: %#x\n", le32toh(opts.magic.value));
+    printf("The magic number is: %#x\n", le32toh(magic.header.value));
 
     printf("The magic number as a string: ");
 
     // Print each character of magic_header
     for (size_t i = 0; i < MAGIC_HEADER; ++i) {
-        if (opts.magic.header[i] != '\0') {
-            putchar(opts.magic.header[i]);
+        if (magic.header.name[i] != '\0') {
+            putchar(magic.header.name[i]);
         }
     }
 
     printf("\n");
 
     // Close the binary file
-    fclose(opts.magic.file);
+    fclose(magic.file);
 
     return 0;
 }
--- src/magic.h
+++ src/magic.h
@@ -6,29 +6,29 @@
 
 #define MAGIC_HEADER 4
 
-enum magic_file_t {
-    MAGIC_FILE_TYPE_F32,
-    MAGIC_FILE_TYPE_F16,
-    MAGIC_FILE_TYPE_Q8_0,
-    MAGIC_FILE_TYPE_Q4_0,
-    MAGIC_FILE_TYPE_I32,
-    MAGIC_FILE_TYPE_I16,
-    MAGIC_FILE_TYPE_I8,
-    MAGIC_FILE_TYPE_COUNT,
+enum magic_precision_t {
+    MAGIC_PRECISION_F32,
+    MAGIC_PRECISION_F16,
+    MAGIC_PRECISION_Q8_0,
+    MAGIC_PRECISION_Q4_0,
+    MAGIC_PRECISION_I32,
+    MAGIC_PRECISION_I16,
+    MAGIC_PRECISION_I8,
+    MAGIC_PRECISION_COUNT,
 };
 
-enum magic_value_t {
-    MAGIC_VALUE_TYPE_FLOAT32,
-    MAGIC_VALUE_TYPE_FLOAT16,
-    MAGIC_VALUE_TYPE_QUANT8_0,
-    MAGIC_VALUE_TYPE_QUANT4_0,
-    MAGIC_VALUE_TYPE_INT32,
-    MAGIC_VALUE_TYPE_INT16,
-    MAGIC_VALUE_TYPE_INT8,
-    MAGIC_VALUE_TYPE_BOOL,
-    MAGIC_VALUE_TYPE_STRING,
-    MAGIC_VALUE_TYPE_ARRAY,
-    MAGIC_VALUE_TYPE_OBJECT,
+enum magic_data_t {
+    MAGIC_DATA_TYPE_FLOAT32,
+    MAGIC_DATA_TYPE_FLOAT16,
+    MAGIC_DATA_TYPE_QUANT8_0,
+    MAGIC_DATA_TYPE_QUANT4_0,
+    MAGIC_DATA_TYPE_INT32,
+    MAGIC_DATA_TYPE_INT16,
+    MAGIC_DATA_TYPE_INT8,
+    MAGIC_DATA_TYPE_BOOL,
+    MAGIC_DATA_TYPE_STRING,
+    MAGIC_DATA_TYPE_ARRAY,
+    MAGIC_DATA_TYPE_OBJECT,
 };
 
 struct magic_string_t {
@@ -36,33 +36,40 @@ struct magic_string_t {
     char*             string;
 };
 
-union magic_data_t {
-    float                 float32;
-    _Float16              float16;
-    int32_t               int32;
-    int16_t               int16;
-    int8_t                int8;
-    bool                  boolean;
+union magic_value_t {
+    _Float32 float32;
+    _Float16 float16;
+    int32_t  int32;
+    int16_t  int16;
+    int8_t   int8;
+    bool     boolean;
+
     struct magic_string_t string;
+
+    struct magic_array_t {
+        magic_data_t         type;
+        size_t               length;
+        union magic_value_t* elements;
+    };
 };
 
-struct magic_array_t {
-    magic_value_t       type;
-    size_t              length;
-    union magic_data_t* array;
+struct magic_kv_t {
+    magic_string_t key;
+    magic_data_t   value_type;
+    magic_value_t  value;
+    size_t         length;
 };
 
 struct magic_header_t {
-    char         header[MAGIC_HEADER];
+    char         name[MAGIC_HEADER];
     unsigned int value;
     unsigned int version;
-    unsigned int tensor_count;
 };
 
-struct magic_file {
-    char*                  path;
-    FILE*                  file;
-    struct magic_header_t* header;
+struct magic_file_t {
+    char*                 path;
+    FILE*                 file;
+    struct magic_header_t header;
 };
 
 #endif // AUTO_MOD_MAGIC_H
