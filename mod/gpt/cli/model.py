633eba65a268dfe1cdb2073af67f45b27a4ce640
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-10 14:51:25 -0400

    refactor: Add function for encapsulating args and for printing csv formatting content

--- mod/gpt/cli/model.py
+++ mod/gpt/cli/model.py
@@ -3,25 +3,44 @@ import torch
 import argparse
 import texttable
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "model_name_or_path",
-    default="openai-community/gpt2",
-    help="The local or remote path to the GPT-2 model",
-)
-parser.add_argument(
-    "-t",
-    "--tensors",
-    action="store_true",
-    help="Dump the tensor metadata to standard output",
-)
-parser.add_argument(
-    "-n",
-    "--tensor-name",
-    help="Dump the tensor view to standard output by name.",
-)
-args = parser.parse_args()
 
+def print_csv(csv_table: list[list[str]], width: int = 240) -> None:
+    """Prints a formatted table of the CSV contents to the console.
+
+    Args:
+        csv_table: The table to print.
+        width (optional): The width of the table. Defaults to 240.
+    """
+    tt = texttable.Texttable(width)
+    tt.set_deco(texttable.Texttable.HEADER)
+    tt.set_cols_dtype(["t"] * len(csv_table[0]))
+    tt.set_cols_align(["r"] * len(csv_table[0]))
+    tt.add_rows(csv_table)
+    print(tt.draw())
+
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "model_name_or_path",
+        default="openai-community/gpt2",
+        help="The local or remote path to the GPT-2 model",
+    )
+    parser.add_argument(
+        "-t",
+        "--tensors",
+        action="store_true",
+        help="Dump the tensor metadata to standard output",
+    )
+    parser.add_argument(
+        "-n",
+        "--tensor-name",
+        help="Dump the tensor view to standard output by name.",
+    )
+    return parser.parse_args()
+
+
+args = get_arguments()
 print("Model:", args.model_name_or_path)
 gpt = GPT2LMHeadModel.from_pretrained(args.model_name_or_path)
 state = gpt.state_dict()
