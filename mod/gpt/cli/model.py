6614e8a32bf4f2ecda544d8d6c601eae77054c91
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-10 14:23:01 -0400

    feat: Add CLI for flexible path to GPT model

--- mod/gpt/cli/model.py
+++ mod/gpt/cli/model.py
@@ -1,7 +1,17 @@
 from transformers import GPT2Config, GPT2LMHeadModel
 import torch
+import argparse
 
-gpt = GPT2LMHeadModel.from_pretrained("/mnt/valerie/models/openai/gpt2")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "model_name_or_path",
+    default="openai-community/gpt2",
+    help="The local or remote path to the GPT-2 model",
+)
+args = parser.parse_args()
+gpt = GPT2LMHeadModel.from_pretrained(args.model_name_or_path)
+
+# Dump the model tensor information
 state = gpt.state_dict()
 for k, v in state.items():
     if isinstance(v, torch.Tensor):
